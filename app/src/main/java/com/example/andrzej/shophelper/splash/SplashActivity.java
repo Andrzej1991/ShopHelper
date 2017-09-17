2b4efbc4d9c33758bcd607c3d47fdec074c93099
Andrzej <andrzej.perski@hotmail.com>
2017-09-17 19:22:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/andrzej/shophelper/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.andrzej.shophelper.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andrzej.shophelper.MainActivity;
+import com.example.andrzej.shophelper.R;
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+        splashDelay();
+    }
+
+    private void splashDelay(){
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
