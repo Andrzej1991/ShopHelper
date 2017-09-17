bbaa92e89ce4075993f9ef1eec4656de16afd3b3
Andrzej <andrzej.perski@hotmail.com>
2017-09-17 20:04:45 +0200

    add layout of main activity with two buttons

---
+++ app/src/main/java/com/example/andrzej/shophelper/model/Order.java
@@ -0,0 +1,54 @@
+package com.example.andrzej.shophelper.model;
+
+/**
+ * Created by Andrzej on 2017-09-17.
+ */
+
+public class Order {
+
+    private int quantity;
+    private String name;
+    private String address;
+    private String description;
+    private String numberOfLanding;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNumberOfLanding() {
+        return numberOfLanding;
+    }
+
+    public void setNumberOfLanding(String numberOfLanding) {
+        this.numberOfLanding = numberOfLanding;
+    }
+}
